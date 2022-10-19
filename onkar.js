463b8c188bde80ddce369584ed34dcb99e43a04a
OnkarNalawade <112932309+OnkarNalawade@users.noreply.github.com>
2022-10-19 23:50:39 +0530

    Add files via upload
    Tindog website

---
+++ onkar.js
@@ -0,0 +1,10 @@
+var age =prompt("enter your age");
+// var b = 90-age;
+lifecount(age); {
+var x=365*age;
+var y=52*age;
+var z=12*age;
+var q= console.log("you have"+x+"days ,"+ y + "weeks ," +z+ "months left.")
+
+
+}
